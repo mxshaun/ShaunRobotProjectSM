2556d399398be55364bbbe89073f3b0ec3cd7c06
Shaun Rouwen <shaun@rouwen.eu>
2018-10-17 14:44:56 +0200

    logging toegevoegd (sam) en lineFollower

--- C13-D/src/nl/hva/miw/robot/cohort13/LineFollower.java
+++ C13-D/src/nl/hva/miw/robot/cohort13/LineFollower.java
@@ -6,68 +6,105 @@ import lejos.hardware.lcd.LCD;
 import lejos.hardware.motor.*;
 import lejos.hardware.port.*;
 import lejos.robotics.Color;
-import ev3.robotproject.library.ColorSensor;
-import ev3.robotproject.library.TouchSensor;
+import ev3.robotproject.library.*;
+import java.io.IOException;
 
-public class LineFollower 
-{ 
-    static UnregulatedMotor motorA = new UnregulatedMotor(MotorPort.A);
-    static UnregulatedMotor motorB = new UnregulatedMotor(MotorPort.B);
-    static TouchSensor        touch = new TouchSensor(SensorPort.S1);
-    static ColorSensor        color = new ColorSensor(SensorPort.S3);
-    
-    public static void main(String[] args)
-    {
-        float    colorValue;
-        
-        System.out.println("Line Follower\n");
-        
-        color.setRedMode();
-        color.setFloodLight(Color.RED);
-        color.setFloodLight(true);
+public class LineFollower {
+	static UnregulatedMotor motorA = new UnregulatedMotor(MotorPort.A);
+	static UnregulatedMotor motorB = new UnregulatedMotor(MotorPort.B);
+	static TouchSensor touch = new TouchSensor(SensorPort.S1);
+	static ColorSensor color = new ColorSensor(SensorPort.S3);
 
-        Button.LEDPattern(4);    // flash green led and 
-        Sound.beepSequenceUp();  // make sound when ready.
+	public static void main(String[] args) throws Exception {
+		Logging.setup(LineFollower.class.getPackage(), false);
+		Logging.log("Start");
+		float colorValue;
 
-        System.out.println("Press any key to start");
-        
-        Button.waitForAnyPress();
-        
-        motorA.setPower(40);
-        motorB.setPower(40);
-       
-        // drive waiting for touch sensor or escape key to stop driving.
+		System.out.println("Line Follower\n");
 
-        while (!touch.isTouched() && Button.ESCAPE.isUp()) 
-        {
-            colorValue = color.getRed();
-            
-            LCD.clear(7);
-            
+		color.setRedMode();
+		color.setFloodLight(Color.RED);
+		color.setFloodLight(true);
 
-            if (colorValue > .100)
-            {
-                motorA.setPower(40);
-                motorB.setPower(20);
-            }
-            else
-            {
-                motorA.setPower(20);    
-                motorB.setPower(40);
-            }
-        }
-       
-        // stop motors with brakes on.
-        motorA.stop();
-        motorB.stop();
+		Button.LEDPattern(4); // flash green led and
+		Sound.beepSequenceUp(); // make sound when ready.
 
-        // free up resources.
-        motorA.close();
-        motorB.close();
-        touch.close();
-        color.close();
-       
-        Sound.beepSequence(); // we are done.
-    }
+		System.out.println("Press any key to start");
+
+		Button.waitForAnyPress();
+		Logging.log("Button is pressed");
+		motorA.setPower(40);
+		motorB.setPower(40);
+
+		// drive waiting for touch sensor or escape key to stop driving.
+
+		while (!touch.isTouched() && Button.ESCAPE.isUp()) {
+			
+			colorValue = color.getRed();
+			Logging.log("gemeten colorvalue: %f",  colorValue);
+			LCD.clear(7);
+
+			if (colorValue > .35) {
+				if (colorValue > .70) {
+					motorA.setPower(90);
+					motorB.setPower(-10);
+				} else if (colorValue > .65) {
+					motorA.setPower(90);
+					motorB.setPower(0);
+				} else if (colorValue > .60) {
+					motorA.setPower(80);
+					motorB.setPower(10);
+				} else if (colorValue > .55) {
+					motorA.setPower(70);
+					motorB.setPower(20);
+				} else if (colorValue > .50) {
+					motorA.setPower(60);
+					motorB.setPower(20);
+				} else if (colorValue > .45) {
+					motorA.setPower(50);
+					motorB.setPower(20);
+				} else if (colorValue > .40) {
+					motorA.setPower(40);
+					motorB.setPower(20);
+				}
+				motorA.setPower(40);
+				motorB.setPower(20);
+			} else {
+				if (colorValue < .30) {
+					motorA.setPower(20);
+					motorB.setPower(50);
+				} else if (colorValue < .25) {
+					motorA.setPower(20);
+					motorB.setPower(60);
+				} else if (colorValue < .20) {
+					motorA.setPower(20);
+					motorB.setPower(70);
+				} else if (colorValue < .15) {
+					motorA.setPower(20);
+					motorB.setPower(80);
+				} else if (colorValue < .10) {
+					motorA.setPower(0);
+					motorB.setPower(90);
+				} else if (colorValue < .05) {
+					motorA.setPower(-10);
+					motorB.setPower(90);
+				}
+				Logging.log("Moterkrcht A: %d Motorkracht B: %d", motorA.getPower(), motorB.getPower());
+				motorA.setPower(20);
+				motorB.setPower(40);
+			}
+		}
+
+		// stop motors with brakes on.
+		motorA.stop();
+		motorB.stop();
+
+		// free up resources.
+		motorA.close();
+		motorB.close();
+		touch.close();
+		color.close();
+
+		Sound.beepSequence(); // we are done.
+	}
 }
-   
