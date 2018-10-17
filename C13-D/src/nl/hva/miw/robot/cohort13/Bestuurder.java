b7466375575d42c7718c564fe7305614e5118add
S. Jurriaans <sam.jurriaans@hva.nl>
2018-10-17 22:14:40 +0200

    nu inclusief bestuurdersklasse (die hoogstwaarschijnlijk weggedingest wordt)

---
+++ C13-D/src/nl/hva/miw/robot/cohort13/Bestuurder.java
@@ -0,0 +1,12 @@
+package nl.hva.miw.robot.cohort13;
+
+import lejos.hardware.motor.*;
+import lejos.hardware.port.*;
+import ev3.robotproject.library.Motor;
+
+public class Bestuurder {
+	// declareren en initialiseren van de motor
+
+	
+
+}
