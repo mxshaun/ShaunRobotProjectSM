3514f98f359a2c7dfeea80a85dcb9019824089eb
S. Jurriaans <sam.jurriaans@hva.nl>
2018-10-17 22:13:13 +0200

    Utility class motor gefixt

--- C13-D/src/ev3/robotproject/library/Motor.java
+++ C13-D/src/ev3/robotproject/library/Motor.java
@@ -3,13 +3,52 @@ package ev3.robotproject.library;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 
-public class Motor {
+public final class Motor {
 	
-	// declareren en initialiseren van het rechter wiel motor (MOTOR_A) en het linker wiel motor (MOTOR_B):
-	final static EV3LargeRegulatedMotor MOTOR_RECHTERWIEL = new EV3LargeRegulatedMotor(MotorPort.A);
-	final static EV3LargeRegulatedMotor MOTOR_LINKERWIEL = new EV3LargeRegulatedMotor(MotorPort.B);
+	// declareren van het rechter wiel motor (MOTOR_A) en het linker wiel motor (MOTOR_B):
+	final static EV3LargeRegulatedMotor MOTOR_LINKER = new EV3LargeRegulatedMotor(MotorPort.A);
+	final static EV3LargeRegulatedMotor MOTOR_RECHTER = new EV3LargeRegulatedMotor(MotorPort.B);
 	
-
+	private Motor() {
+	}
+	
+	// input is in graden per seconde
+	public static void rechtVooruit(int vermogen) {
+		MOTOR_LINKER.setSpeed(vermogen);
+		MOTOR_RECHTER.setSpeed(vermogen);
+		MOTOR_LINKER.forward();
+		MOTOR_RECHTER.forward();
+		
+	}
+	
+	// input is in graden per seconde
+	public static void bochtVooruit(int vermogenLinks, int vermogenRechts) {
+		MOTOR_LINKER.setSpeed(vermogenLinks);
+		MOTOR_RECHTER.setSpeed(vermogenRechts);
+		MOTOR_LINKER.forward();
+		MOTOR_RECHTER.forward();
+	}
+	
+	// input is in graden per seconde
+	public static void rechtAchteruit(int vermogen) {
+		MOTOR_LINKER.setSpeed(vermogen);
+		MOTOR_RECHTER.setSpeed(vermogen);
+		MOTOR_LINKER.backward();
+		MOTOR_RECHTER.backward();
+	}
+	
+	// input is in graden per seconde
+	public static void bochtAchteruit(int vermogenLinks, int vermogenRechts) {
+		MOTOR_LINKER.setSpeed(vermogenLinks);
+		MOTOR_RECHTER.setSpeed(vermogenRechts);
+		MOTOR_LINKER.backward();
+		MOTOR_RECHTER.backward();
+	}
+	
+	public static void sluit() {
+		MOTOR_LINKER.close();
+		MOTOR_RECHTER.close();
+	}
 }
 
 
--- C13-D/src/main/RobotLauncher.java
+++ C13-D/src/main/RobotLauncher.java
@@ -1,10 +1,12 @@
 package main;
 
+import nl.hva.miw.robot.cohort13.Bestuurder;
+
 public class RobotLauncher {
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
