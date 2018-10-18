4f1921d8bfe1c7dde58748ef846e976943512cf7
Shaun Rouwen <shaun@rouwen.eu>
2018-10-18 10:50:57 +0200

    linefolllower aangepast naar regulatedmotors en terugdraaien wielen

--- C13-D/src/nl/hva/miw/robot/cohort13/LineFollower.java
+++ C13-D/src/nl/hva/miw/robot/cohort13/LineFollower.java
@@ -10,10 +10,12 @@ import ev3.robotproject.library.*;
 import java.io.IOException;
 
 public class LineFollower {
-	static UnregulatedMotor motorA = new UnregulatedMotor(MotorPort.A);
-	static UnregulatedMotor motorB = new UnregulatedMotor(MotorPort.B);
+	//static UnregulatedMotor motorA = new UnregulatedMotor(MotorPort.A);
+	//static UnregulatedMotor motorB = new UnregulatedMotor(MotorPort.B);
 	static TouchSensor touch = new TouchSensor(SensorPort.S1);
 	static ColorSensor color = new ColorSensor(SensorPort.S3);
+	static EV3LargeRegulatedMotor motorAA = new EV3LargeRegulatedMotor(MotorPort.A);
+	static EV3LargeRegulatedMotor motorBB = new EV3LargeRegulatedMotor(MotorPort.B);
 
 	public static void main(String[] args) throws Exception {
 		Logging.setup(LineFollower.class.getPackage(), false);
@@ -22,7 +24,7 @@ public class LineFollower {
 		float colorValueBlack;
 		float colorValue;
 		float colorBorder;
-
+		
 		System.out.println("Line Follower\n");
 
 		color.setRedMode();
@@ -56,8 +58,14 @@ public class LineFollower {
 
 		System.out.println("Press any key to start the race");
 		Button.waitForAnyPress();
-		motorA.setPower(25);
-		motorB.setPower(25);
+		float maxSpeedMotorA = motorAA.getMaxSpeed(); 
+		float maxSpeedMotorB = motorBB.getMaxSpeed();
+		
+		motorAA.setSpeed(maxSpeedMotorA / 50);
+		motorBB.setSpeed(maxSpeedMotorB / 50);
+		motorAA.forward();
+		motorBB.forward();
+		//motorB.setPower(25);
 
 		// drive waiting for touch sensor or escape key to stop driving.
 
@@ -66,65 +74,105 @@ public class LineFollower {
 			colorValue = color.getRed();
 			Logging.log("gemeten colorvalue: %f",  colorValue);
 			LCD.clear(7);
+			float maxSpeedMotorAA = motorAA.getMaxSpeed();
+			float maxSpeedMotorBB = motorBB.getMaxSpeed();
 
 			if (colorValue > colorBorder) {
 				if (colorValue/colorBorder > 1.7) {
-					motorA.setPower(90);
-					motorB.setPower(0);
+					motorAA.setSpeed(maxSpeedMotorAA * (float)0.9);
+					motorBB.setSpeed(maxSpeedMotorBB * (float)0.4);
+					motorAA.forward();
+					motorBB.backward();
 				} else if (colorValue/colorBorder > 1.6) {
-					motorA.setPower(90);
-					motorB.setPower(0);
+					motorAA.setSpeed(maxSpeedMotorAA * (float)0.8);
+					motorBB.setSpeed(maxSpeedMotorBB * (float)0.2);
+
+					motorAA.forward();
+					motorBB.backward();
 				} else if (colorValue/colorBorder > 1.5) {
-					motorA.setPower(80);
-					motorB.setPower(10);
+					motorAA.setSpeed(maxSpeedMotorAA * (float)0.4);
+					motorBB.setSpeed(maxSpeedMotorBB * (float)0.1);
+
+					motorAA.forward();
+					motorBB.backward();
 				} else if (colorValue/colorBorder > 1.4) {
-					motorA.setPower(70);
-					motorB.setPower(10);
+					motorAA.setSpeed(maxSpeedMotorAA * (float)0.4);
+					motorBB.setSpeed(maxSpeedMotorBB * (float)0.0);
+
+					motorAA.forward();
+					motorBB.forward();
 				} else if (colorValue/colorBorder > 1.3) {
-					motorA.setPower(60);
-					motorB.setPower(10);
+					motorAA.setSpeed(maxSpeedMotorAA * (float)0.3);
+					motorBB.setSpeed(maxSpeedMotorBB * (float)0.1);
+
+					motorAA.forward();
+					motorBB.forward();
 				} else if (colorValue/colorBorder > 1.2) {
-					motorA.setPower(50);
-					motorB.setPower(10);
-				} else if (colorValue/colorBorder > 1.1) {
-					motorA.setPower(40);
-					motorB.setPower(10);
-				}
-				motorA.setPower(20);
-				motorB.setPower(10);
+					motorAA.setSpeed(maxSpeedMotorAA * (float)0.2);
+					motorBB.setSpeed(maxSpeedMotorBB * (float)0.1);
+
+					motorAA.forward();
+					motorBB.forward();
+				} 
+				motorAA.setSpeed(maxSpeedMotorAA * (float)0.2);
+				motorBB.setSpeed(maxSpeedMotorBB * (float)0.1);
+
+				motorAA.forward();
+				motorBB.forward();
 			} else {
 				if (colorValue/colorBorder < 0.40) {
-					motorA.setPower(0);
-					motorB.setPower(90);
+					motorAA.setSpeed(maxSpeedMotorAA * (float)0.4);
+					motorBB.setSpeed(maxSpeedMotorBB * (float)0.9);
+
+					motorAA.backward();
+					motorBB.forward();
 				} else if (colorValue/colorBorder < 0.50) {
-					motorA.setPower(0);
-					motorB.setPower(80);
+					motorAA.setSpeed(maxSpeedMotorAA * (float)0.2);
+					motorBB.setSpeed(maxSpeedMotorBB * (float)0.8);
+
+					motorAA.backward();
+					motorBB.forward();
 				} else if (colorValue/colorBorder < 0.60) {
-					motorA.setPower(10);
-					motorB.setPower(70);
+					motorAA.setSpeed(maxSpeedMotorAA * (float)0.1);
+					motorBB.setSpeed(maxSpeedMotorBB * (float)0.4);
+
+					motorAA.backward();
+					motorBB.forward();
 				} else if (colorValue/colorBorder < 0.70) {
-					motorA.setPower(10);
-					motorB.setPower(60);
+					motorAA.setSpeed(maxSpeedMotorAA * (float)0.0);
+					motorBB.setSpeed(maxSpeedMotorBB * (float)0.4);
+
+					motorAA.forward();
+					motorBB.forward();
 				} else if (colorValue/colorBorder < 0.80) {
-					motorA.setPower(10);
-					motorB.setPower(50);
+					motorAA.setSpeed(maxSpeedMotorAA * (float)0.1);
+					motorBB.setSpeed(maxSpeedMotorBB * (float)0.3);
+
+					motorAA.forward();
+					motorBB.forward();
 				} else if (colorValue/colorBorder < 0.90) {
-					motorA.setPower(10);
-					motorB.setPower(40);
+					motorAA.setSpeed(maxSpeedMotorAA * (float)0.1);
+					motorBB.setSpeed(maxSpeedMotorBB * (float)0.2);
+
+					motorAA.forward();
+					motorBB.forward();
 				}
-				Logging.log("Moterkrcht A: %d Motorkracht B: %d", motorA.getPower(), motorB.getPower());
-				motorA.setPower(10);
-				motorB.setPower(20);
+				Logging.log("Moterkrcht A: %d Motorkracht B: %d", motorAA.getSpeed(), motorBB.getSpeed());
+				motorAA.setSpeed(maxSpeedMotorAA * (float)0.1);
+				motorBB.setSpeed(maxSpeedMotorBB * (float)0.2);
+
+				motorAA.forward();
+				motorBB.forward();
 			}
 		}
 
 		// stop motors with brakes on.
-		motorA.stop();
-		motorB.stop();
+		motorAA.stop();
+		motorBB.stop();
 
 		// free up resources.
-		motorA.close();
-		motorB.close();
+		motorAA.close();
+		motorBB.close();
 		touch.close();
 		color.close();
 
